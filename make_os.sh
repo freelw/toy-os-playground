f62ec771d26c6986933cdc6b8a98ab9828d104d3
wangli <freelw81@qq.com>
2025-10-30 22:01:18 +0800

    update

--- make_os.sh
+++ make_os.sh
@@ -5,7 +5,7 @@ source ./lan_os_rc
 cd src &&
 echo 'builing lan os...'
 lan_make > /dev/null 2>&1 &&
-cp a.vfd ../../ &&
+cp a.vfd ../ &&
 echo 'cleaning temp files...'
 lan_clean > /dev/null 2>&1
 echo "file [a.vfd] is our os image with a simple ramdisk filesystem."
