2f062e09a824639bf0d9c4df3cd7fadf8cf4188b
wangli <freelw81@qq.com>
2025-11-07 22:31:25 +0800

    update

--- tool/makeall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --rm -v `pwd`/:/share -v ${lan_base_dir}/demos/common_header_demo/common:/common freelw/lan_make:latest
--- tool/makeclean.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --rm -v `pwd`/:/share freelw/lan_clean:latest 
