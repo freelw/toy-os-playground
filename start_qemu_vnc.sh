b79a320bdcef642ab175b1fc434f67f187a7fcef
wangli <freelw81@qq.com>
2025-11-07 22:33:52 +0800

    update

--- start_qemu_vnc.sh
+++ start_qemu_vnc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-( echo 'change vnc password' ; sleep 1 ; echo '0' ) | qemu-system-x86_64 -vnc :0,password=on -monitor stdio -drive file=./a.vfd,index=0,if=floppy,format=raw -boot a
+( echo 'change vnc password' ; sleep 1 ; echo '0' ) | qemu-system-x86_64 -vnc :0,password=on -monitor stdio -drive file=./src/a.vfd,index=0,if=floppy,format=raw -boot a
